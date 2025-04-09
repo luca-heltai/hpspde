c20190ddb9d93d0fea82caedef36b0ee9df6fcde
Luca Heltai <luca.heltai@unipi.it>
2025-04-09 13:55:58 +0200

    added luca and davide

---
+++ include/test_bench.h
@@ -0,0 +1,71 @@
+#ifndef TEST_BENCH_H
+#define TEST_BENCH_H
+
+#include <deal.II/base/function_parser.h>
+#include <deal.II/base/parameter_handler.h>
+#include <deal.II/base/parsed_convergence_table.h>
+
+#include <deal.II/dofs/dof_handler.h>
+
+#include <deal.II/fe/fe_q.h>
+
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/vector.h>
+
+#include <string>
+
+using namespace dealii;
+
+template <int dim, int spacedim = dim>
+struct TestBenchParameters
+{
+  TestBenchParameters(const std::string &filename = "test_bench.prm");
+
+  unsigned int initial_refinement        = 1;
+  unsigned int n_cycles                  = 1;
+  std::string  fe_name                   = "FE_Q(1)";
+  std::string  exact_solution_expression = "sin(2*pi*x)*sin(2*pi*y)";
+  std::string  grid_name                 = "hyper_cube";
+  std::string  grid_arguments            = "0 : 1 : false";
+  std::string  output_file_name          = "solution";
+  std::string  output_directory          = ".";
+  std::vector<std::string> renumbering;
+
+  FunctionParser<spacedim>       exact_solution;
+  mutable ParsedConvergenceTable convergence_table;
+
+  ParameterHandler prm;
+};
+
+template <int dim, int spacedim = dim>
+class TestBench
+{
+public:
+  TestBench(const TestBenchParameters<dim, spacedim> &parameters);
+  void
+  initialize();
+  void
+  make_grid();
+  void
+  setup_system();
+
+  Vector<double>
+  make_vector() const;
+
+  void
+  interpolate();
+  void
+  output_results(const Vector<double> &output_field,
+                 const std::string    &suffix) const;
+
+  const TestBenchParameters<dim, spacedim> &par;
+
+  Triangulation<dim, spacedim>                  triangulation;
+  std::unique_ptr<FiniteElement<dim, spacedim>> fe;
+  DoFHandler<dim, spacedim>                     dof_handler;
+
+  Vector<double> solution;
+};
+
+#endif // TestBench_H
---
+++ source/test_bench.cc
@@ -0,0 +1,179 @@
+#include "test_bench.h"
+
+#include <deal.II/dofs/dof_renumbering.h>
+
+#include <deal.II/fe/fe_tools.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_tools.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/vector_tools.h>
+
+// Definitions for TestBenchParameters
+template <int dim, int spacedim>
+TestBenchParameters<dim, spacedim>::TestBenchParameters(
+  const std::string &filename)
+{
+  prm.enter_subsection("TestBench parameters");
+  {
+    prm.add_parameter("Finite element name", fe_name);
+    prm.add_parameter("Initial refinement", initial_refinement);
+    prm.add_parameter("Exact solution expression", exact_solution_expression);
+    prm.add_parameter("Grid name", grid_name);
+    prm.add_parameter("Grid arguments", grid_arguments);
+    prm.add_parameter("Output file name", output_file_name);
+    prm.add_parameter("Output directory", output_directory);
+    prm.add_parameter("DoF renumbering", renumbering);
+  }
+  prm.leave_subsection();
+
+  prm.enter_subsection("Convergence table");
+  convergence_table.add_parameters(prm);
+  prm.leave_subsection();
+
+  try
+    {
+      prm.parse_input(filename);
+    }
+  catch (std::exception &exc)
+    {
+      prm.print_parameters(filename, ParameterHandler::Short);
+      prm.parse_input(filename);
+    }
+  std::map<std::string, double> constants;
+  constants["pi"] = numbers::PI;
+  exact_solution.initialize(FunctionParser<dim>::default_variable_names(),
+                            {exact_solution_expression},
+                            constants);
+}
+
+// Definitions for TestBench
+template <int dim, int spacedim>
+TestBench<dim, spacedim>::TestBench(
+  const TestBenchParameters<dim, spacedim> &par)
+  : par(par)
+  , dof_handler(triangulation)
+{}
+
+template <int dim, int spacedim>
+void
+TestBench<dim, spacedim>::make_grid()
+{
+  GridGenerator::generate_from_name_and_arguments(triangulation,
+                                                  par.grid_name,
+                                                  par.grid_arguments);
+  triangulation.refine_global(par.initial_refinement);
+}
+
+
+template <int dim, int spacedim>
+void
+TestBench<dim, spacedim>::setup_system()
+{
+  fe = FETools::get_fe_by_name<dim, spacedim>(par.fe_name);
+  dof_handler.distribute_dofs(*fe);
+  for (const auto &renumber : par.renumbering)
+    {
+      if (renumber == "Cuthill_McKee")
+        DoFRenumbering::Cuthill_McKee(dof_handler);
+      else if (renumber == "component_wise")
+        DoFRenumbering::component_wise(dof_handler);
+      else if (renumber == "support_point_wise")
+        DoFRenumbering::support_point_wise(dof_handler);
+      else if (renumber == "block_wise")
+        DoFRenumbering::block_wise(dof_handler);
+      else if (renumber == "hierarchical")
+        DoFRenumbering::hierarchical(dof_handler);
+      else if (renumber == "lexicographic")
+        {
+          if constexpr (dim > 1 && dim == spacedim)
+            {
+              BoundingBox<spacedim> bounding_box(triangulation.get_vertices());
+              const auto max = bounding_box.get_boundary_points().second.norm();
+              Tensor<1, spacedim> direction;
+              for (unsigned int i = 0; i < spacedim; ++i)
+                direction[spacedim - i - 1] = (std::pow(10u, i)) * max;
+
+              DoFRenumbering::downstream(dof_handler, direction, true);
+            }
+          else
+            {
+              AssertThrow(false,
+                          ExcMessage("Not implemented for dim/spacedim: " +
+                                     renumber));
+            }
+        }
+      else
+        {
+          AssertThrow(false,
+                      ExcMessage("Unknown renumbering strategy: " + renumber));
+        }
+    }
+}
+
+template <int dim, int spacedim>
+auto
+TestBench<dim, spacedim>::make_vector() const -> Vector<double>
+{
+  return Vector<double>(dof_handler.n_dofs());
+};
+
+
+
+template <int dim, int spacedim>
+void
+TestBench<dim, spacedim>::interpolate()
+{
+  VectorTools::interpolate(dof_handler, par.exact_solution, solution);
+  par.convergence_table.error_from_exact(dof_handler,
+                                         solution,
+                                         par.exact_solution);
+}
+
+
+template <int dim, int spacedim>
+void
+TestBench<dim, spacedim>::output_results(const Vector<double> &output_field,
+                                         const std::string    &suffix) const
+{
+  DataOutBase::VtkFlags vtk_flags;
+  vtk_flags.write_higher_order_cells = true;
+  DataOut<dim, spacedim> data_out;
+  data_out.set_flags(vtk_flags);
+
+  data_out.attach_dof_handler(dof_handler);
+  data_out.add_data_vector(output_field, "solution");
+
+  data_out.build_patches();
+  std::string fname =
+    par.output_file_name + (suffix == "" ? "" : "_" + suffix) + ".vtu";
+
+  std::ofstream vtuoutput(fname);
+  data_out.write_vtu(vtuoutput);
+}
+
+
+template <int dim, int spacedim>
+void
+TestBench<dim, spacedim>::initialize()
+{
+  make_grid();
+  setup_system();
+}
+
+
+// Explicit instantiations
+template struct TestBenchParameters<1, 1>;
+template struct TestBenchParameters<1, 2>;
+template struct TestBenchParameters<1, 3>;
+template struct TestBenchParameters<2, 2>;
+template struct TestBenchParameters<2, 3>;
+template struct TestBenchParameters<3, 3>;
+
+template class TestBench<1, 1>;
+template class TestBench<1, 2>;
+template class TestBench<1, 3>;
+template class TestBench<2, 2>;
+template class TestBench<2, 3>;
+template class TestBench<3, 3>;
